25ba008278bab7e13f9ab34663813767006808d3
Shad Anwar <shad93709@gmail.com>
2025-10-04 15:45:26 +0530

    Updated codes

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express"
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from "./config/db.js"
+import authRoutes from './routes/authRoutes.js'
+import leaveRoutes from './routes/leaveRoutes.js'
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.use('/api/auth',authRoutes)
+app.use('/api/leaves',leaveRoutes)
+app.get('/',(req,res)=>{
+    res.send("Leave management API System is running...")
+})
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+});
